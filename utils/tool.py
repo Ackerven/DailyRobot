7135bab545522eb7d847426d3458c41d618e1e53
Ackerven <ackerven@gmail.com>
2022-12-06 22:06:57 +0800

    feat: encapsulate datascore with tool.DB

--- utils/tool.py
+++ utils/tool.py
@@ -7,6 +7,8 @@
 # @Copyright Copyright(C) 2022 Ackerven All rights reserved.
 import time
 
+from utils.mysql.mysql import MySQL
+
 
 def Singleton(cls):
     _instance = {}
@@ -54,3 +56,62 @@ class Notify:
         self.postman.send(address=self.address,
                           subject=self.TITLE + '{} 第{}次失败名单'.format(time.strftime('%Y/%m/%d', time.localtime()), times),
                           text=text)
+
+
+@Singleton
+class DB:
+    SOURCES = {
+        'MYSQL': 'mysql',
+    }
+
+    def __init__(self, source='mysql', config=None):
+        self.engine = None
+        if source == 'mysql':
+            self.engine = MySQL(config)
+
+    def queryAll(self):
+        """ 查询所有用户
+
+        :return: 用户对象列表
+        """
+        return self.engine.queryAll()
+
+    def queryByName(self, name):
+        """ 通过用户名查询用户
+
+        :param name: 用户名
+        :return: 用户对象
+        """
+        return self.engine.queryByName(name)
+
+    def queryById(self, id_):
+        """ 通过 ID 查询用户
+
+        :param id_: 用户 ID
+        :return: 用户对象
+        """
+        return self.engine.queryById(id_)
+
+    def update(self, user):
+        """ 更新用户
+
+        :param user: 用户对象
+        :return:
+        """
+        self.engine.update(user)
+
+    def delete(self, user):
+        """ 删除用户
+
+        :param user: 用户对象
+        :return:
+        """
+        self.engine.delete(user)
+
+    def insert(self, user):
+        """ 插入用户
+
+        :param user: 用户对象
+        :return:
+        """
+        self.engine.insert(user)
