acf040d763c1ce2ed04278be369dbb06ec6e9bdc
Ackerven <ackerven@gmail.com>
2022-12-09 09:30:16 +0800

    fix: mail throw Connection unexpectedly closed

--- utils/mail.py
+++ utils/mail.py
@@ -20,30 +20,26 @@ class Postman:
         self.password = config['password']
         self.port = config['port']
         self.debugMode = config['debug']
-        self.smtp = None
         self.logger = LoggerPool().get()
-        self.init()
 
-    def init(self):
-        self.logger.debug('初始化 Postman 对象')
-        self.smtp = smtplib.SMTP_SSL(host=self.host, port=self.port)
+    def getConnection(self):
+        self.logger.debug('Postman 连接发件服务器')
+        smtp = smtplib.SMTP_SSL(host=self.host, port=self.port)
         if self.debugMode:
-            self.smtp.set_debuglevel(1)
-        self.smtp.ehlo(self.host)
-        self.smtp.login(self.username, self.password)
+            smtp.set_debuglevel(1)
+        # self.smtp.ehlo(self.host)
+        smtp.connect(self.host, self.port)
+        smtp.login(self.username, self.password)
+        return smtp
 
     def send(self, address, *args, **kwargs):
         self.logger.info(f'发送邮件给 {address}')
+        smtp = self.getConnection()
         message = MIMEText(kwargs['text'], 'plain', 'utf-8')
         message['Subject'] = Header(kwargs['subject'])
         message['From'] = Header(self.username)
         message['To'] = Header(address)
-        self.smtp.sendmail(self.username, address, message.as_string())
+        smtp.ehlo(self.host)
+        smtp.sendmail(self.username, address, message.as_string())
         self.logger.info(f'发送邮件给 {address} 成功')
-
-    def __del__(self):
-        try:
-            self.smtp.quit()
-        except Exception as ex:
-            # print(str(ex))
-            pass
+        smtp.quit()
