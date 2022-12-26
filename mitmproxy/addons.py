dcbd478bc84acf55a4fd57bf0dcabcec7a146b34
Ackerven <ackerven@gmail.com>
2022-12-26 22:54:14 +0800

    fix: ignore proxy for primary domain

--- mitmproxy/addons.py
+++ mitmproxy/addons.py
@@ -16,6 +16,8 @@ class Interceptor:
         pass
 
     def request(self, flow: mitmproxy.http.HTTPFlow):
+        if flow.request.host == 'robot.erha.fun':
+            return
         if flow.request.host == 'xxcj.bnuzh.edu.cn' and flow.request.path.startswith('/health-punch'):
             url = flow.request.url
             code = url.split('=')[-1]
@@ -23,10 +25,13 @@ class Interceptor:
             resp = requests.get('http://127.0.0.1:2023/api/auth/{}'.format(code))
             print(resp.text)
             flow.response = http.Response.make(200, resp.text)
+            return
         else:
             # flow.response = http.Response.make(302, headers={'Location': 'https://robot.erha.fun/api/'})
             flow.response = http.Response.make(200,
                                                '此代理仅用于获取打卡认证的code，如需访问其他网站，请先断开代理。\n请访问 http://robot.erha.fun/docs/ 获取更多信息')
+
+        return
 
 
 addons = [Interceptor()]
